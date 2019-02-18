9ffc001b5c8459f65fe96eb22b747eed945c005c
judy <judy@Users-MBP-4.attlocal.net>
2019-02-18 11:27:36 -0800

    CheckPoint: Basic Node App Completed

---
+++ goodbye.js
@@ -0,0 +1,7 @@
+exports.goodbyeSpanish = function(){
+  console.log("Adios");
+}
+
+exports.goodbyeEnglish = function(){
+  console.log("Bye!");
+}
---
+++ main.js
@@ -0,0 +1,16 @@
+const greetings = require("./greeting.js");
+const goodbye = require("./goodbye.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+goodbye.goodbyeSpanish();
+
+goodbye.goodbyeEnglish();
